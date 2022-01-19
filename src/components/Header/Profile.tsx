4051e49a5a9dac0c3286be051111e4676c1a4f71
Vinícius Führ de Borba <tecnologia@tgwsistemas.com.br>
2022-01-19 11:16:01 -0300

    Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="3" textAlign="center">
-        <Text>Vinícius Führ de Borba</Text>
-        <Text color="gray.300" fontSize="small">
-          vinicius@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="3" textAlign="center">
+          <Text>Vinícius Führ de Borba</Text>
+          <Text color="gray.300" fontSize="small">
+            vinicius@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Vinícius Führ de Borba" />
     </Flex>
