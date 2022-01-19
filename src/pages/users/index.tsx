a55d5ed711c42ee7cdf80ee0107399dc8117b896
Vinícius Führ de Borba <tecnologia@tgwsistemas.com.br>
2022-01-19 08:15:59 -0300

    Pagination Component and Form

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,67 +1,91 @@
-import {Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text} from "@chakra-ui/react";
-import {Header} from "../../components/Header";
-import {Sidebar} from "../../components/Sidebar";
-import {RiAddLine, RiPencilLine} from "react-icons/ri";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+} from "@chakra-ui/react";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Pagination } from "../../components/Pagination";
 
-export default function UserList(){
-    return (
-        <Box>
-            <Header/>
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-                <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+            <Button
+              as="a"
+              size="sm"
+              fontSize="small"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
 
-                    <Flex mb="8" justify="space-between" align="center">
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinícius Führ de Borba</Text>
+                    <Text fontSize="small" color="gray.300">
+                      vinicius@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
 
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
-
-                        <Button as="a" size="sm" fontSize="small" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>} >
-                           Criar Novo
-                        </Button>
-
-                    </Flex>
-
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px="6" color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink"></Checkbox>
-                                </Th>
-                                <Th>Usuário</Th>
-                                <Th>Data de Cadastro</Th>
-                                <Th w="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink"></Checkbox>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Vinícius Führ de Borba</Text>
-                                        <Text fontSize="small" color="gray.300">vinicius@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>
-                                    04 de Abril, 2021
-                                </Td>
-                                <Td>
-                                    <Button as="a" size="sm" fontSize="small" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>} >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                </Box>
-
-            </Flex>
-
+          <Pagination />
         </Box>
-    );
+      </Flex>
+    </Box>
+  );
 }
